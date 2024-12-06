fecfec1d97f75ebe9e186addd129b3ed4f3ae54c
Gianluca-Marsala <gianluca.marsala@gmail.com>
2024-12-06 12:26:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,26 @@
+#include <iostream>
 #include <string>
 #include "Dizionario.cpp"
+using namespace std;
 
 int main() {
 
-    Dizionario<std::string> dict;
+    Dizionario<string> dict;
 
     dict.inserisci("apple", "fruit");
     dict.inserisci("car", "vehicle");
     dict.inserisci("book", "object");
-
-    std::cout << "Value for 'apple': " << dict.recupera("apple") << "\n";
-    std::cout << "Value for 'car': " << dict.recupera("car") << "\n";
-    std::cout << "Value for 'pen': " << dict.recupera("pen") << "\n";
-
+    dict.inserisci("pen", "stationery");
+    dict.inserisci("book", "literature");
+    
+    cout << "Stampa del dizionario dopo gli inserimenti:" <<endl;
+    dict.stampa();
+    
+    cout << "Recupero del valore associato a 'apple': " << dict.recupera("apple") <<endl;
+    cout << "Recupero del valore associato a 'car': " << dict.recupera("car") <<endl;
+    cout << "La chiave 'book' appartiene al dizionario? " << (dict.appartiene("book") ? "Sì" : "No") << endl;
+    dict.cancella("car");
+    cout << "Stampa del dizionario dopo la cancellazione di 'car':" <<endl;
     dict.stampa();
 
     return 0;
